612326309d42eb51e7c81d2ac12ee96b099b5a8d
Lucas <iapplelucas2@gmail.com>
2023-08-11 13:48:09 -0300

    Resoluçaõ tarefa do módulo 19

---
+++ tarefasEBAC/modulo19/AnnotationExample.java
@@ -0,0 +1,19 @@
+package tarefasEBAC.modulo19;
+
+import java.lang.annotation.Annotation;
+
+@Tabela(value = "Tarefa EBAC")
+public class AnnotationExample {
+
+    public static void main(String[] args) {
+        Class<?> clazz = AnnotationExample.class;
+
+        if (clazz.isAnnotationPresent(Tabela.class)) {
+            Tabela tabelaAnnotation = clazz.getAnnotation(Tabela.class);
+            String nomeTabela = tabelaAnnotation.value();
+            System.out.println("Nome da Tabela: " + nomeTabela);
+        } else {
+            System.out.println("A anotação @Tabela não está presente na classe.");
+        }
+    }
+}
