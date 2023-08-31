ee041a77a16d5ad835c0b43adb6096aa97f8975f
Lucas <iapplelucas2@gmail.com>
2023-08-31 12:18:01 -0300

    Resoluçaõ tarefa do módulo 24 (implementamndo classe para executar todos os testes)

---
+++ tarefasEBAC/modulo24/teste/AllTest.java
@@ -0,0 +1,11 @@
+package tarefasEBAC.modulo24.teste;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ContratoTest.class,ContratoExceptionTest.class})
+public class AllTest {
+
+}
